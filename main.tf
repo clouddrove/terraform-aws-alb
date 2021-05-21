ff04c6715173877bf1f78a8d1899f9e3fc0c93e9
yadavprakash <yadavprakash.it@gmail.com>
2021-05-21 16:57:26 +0530

    ssl_policy_add

--- main.tf
+++ main.tf
@@ -97,6 +97,7 @@ resource "aws_lb_listener" "nhttps" {
   port              = var.https_listeners[count.index]["port"]
   protocol          = lookup(var.https_listeners[count.index], "protocol", "HTTPS")
   certificate_arn   = var.https_listeners[count.index]["certificate_arn"]
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
   default_action {
     target_group_arn = aws_lb_target_group.main[lookup(var.https_listeners[count.index], "target_group_index", count.index)].id
     type             = "forward"
