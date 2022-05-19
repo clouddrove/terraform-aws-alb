1bb383b5ed051c23634d0b63474f44156f994b61
theprashantyadav <theprashantyadav7@gmail.com>
2022-05-19 17:07:22 +0530

    update version

--- _example/nlb/example.tf
+++ _example/nlb/example.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "clouddrove/vpc/aws"
-  version = "0.15.0"
+  version = "0.15.1"
 
   name        = "vpc"
   environment = "test"
@@ -15,7 +15,7 @@ module "vpc" {
 
 module "public_subnets" {
   source  = "clouddrove/subnet/aws"
-  version = "0.15.0"
+  version = "0.15.3"
 
   name        = "public-subnet"
   environment = "test"
@@ -31,7 +31,7 @@ module "public_subnets" {
 
 module "http-https" {
   source  = "clouddrove/security-group/aws"
-  version = "0.15.0"
+  version = "1.0.1"
 
   name        = "http-https"
   environment = "test"
@@ -45,7 +45,7 @@ module "http-https" {
 
 module "ssh" {
   source  = "clouddrove/security-group/aws"
-  version = "0.15.0"
+  version = "1.0.1"
 
   name        = "ssh"
   environment = "test"
@@ -58,7 +58,7 @@ module "ssh" {
 
 module "iam-role" {
   source  = "clouddrove/iam-role/aws"
-  version = "0.15.0"
+  version = "1.0.1"
 
   name        = "iam-role"
   environment = "test"
@@ -96,7 +96,7 @@ data "aws_iam_policy_document" "iam-policy" {
 
 module "ec2" {
   source  = "clouddrove/ec2/aws"
-  version = "0.15.0"
+  version = "1.0.1"
 
   name        = "ec2-instance"
   environment = "test"
